3c462b0f85cdbb48339b32bdfd85f3adb0b8b99c
Richard Song <9144514+richardmcsong@users.noreply.github.com>
2025-12-10 10:40:25 -0500

    Remove generated OpenAPI schema files and related configurations from the project.

--- workspaces/src/schema/src/schema/openapi/generated/models/index.ts
+++
@@ -1,9 +0,0 @@
-//
-
-export * from '../models/CreateUserRequest.model';
-export * from '../models/Expires.model';
-export * from '../models/ModelError.model';
-export * from '../models/User.model';
-export * from '../models/UserAPIKeyAuth.model';
-export * from '../models/UserKey.model';
-export * from '../models/UserKeyList.model';
